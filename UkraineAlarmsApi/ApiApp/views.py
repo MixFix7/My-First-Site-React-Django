b4edbd09bcb3528551a63bdb9a0832ce1d019328
MixFix7 <mashtalar95@gmail.com>
2023-06-12 16:40:54 +0300

    create react app

--- UkraineAlarmsApi/ApiApp/views.py
+++ UkraineAlarmsApi/ApiApp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from rest_framework.utils.serializer_helpers import ReturnDict
 from rest_framework.views import APIView
 from .models import UkraineAlarmsStatus
 from .serializers import UkraineSerializer
 from rest_framework.response import Response
+from .ukraine_alarms import get_alarms
 
 
 class UkraineAlarmsView(APIView):
@@ -11,6 +13,14 @@ class UkraineAlarmsView(APIView):
         serializer_alarms = UkraineSerializer(alarms, many=True)
         output = serializer_alarms.data
         return Response(output)
+
+    def post(self, request):
+        get_alarms()
+        alarms = UkraineAlarmsStatus.objects.all()
+        serializer_alarms = UkraineSerializer(alarms, many=True)
+        output = serializer_alarms.data
+        return Response(output)
+
 
 
 
